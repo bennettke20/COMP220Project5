2644a0faecea29e2d3e4ba85496efb7d0d62585c
BENNETTKE20 <katherine.e.bennett02@gmail.com>
2021-12-06 20:02:37 -0500

    comments updated

--- src/main/java/com/files/comp220project5/ReadOnlyText.java
+++ src/main/java/com/files/comp220project5/ReadOnlyText.java
@@ -6,49 +6,81 @@ package com.files.comp220project5;
  */
 
 public class ReadOnlyText extends TextBlock {
+    /**
+     * Constructs a blank read-only text document
+     */
     public ReadOnlyText() {
         super();
     }
 
+    /**
+     * Constructs a read-only text document containing a starter string
+     * @param starter String to created the document from
+     */
     public ReadOnlyText(String starter) {
         super(starter);
     }
 
+    /**
+     * Overrides deleteTextBack command to render it ineffectual
+     */
     @Override
     public void deleteTextBack() {
         // DO NOTHING
     }
 
+    /**
+     * Overrides deleteTextForward command to render it ineffectual
+     */
     @Override
     public void deleteTextForward() {
         // DO NOTHING
     }
 
+    /**
+     * Overrides insertText command to render it ineffectual
+     */
     @Override
     public void insertText(char toAdd) {
         // DO NOTHING
     }
 
+    /**
+     * Overrides moveCursorBackward command to render it ineffectual
+     */
     @Override
     public void moveCursorBackward() {
         // DO NOTHING
     }
 
+    /**
+     * Overrides moveCursorForward command to render it ineffectual
+     */
     @Override
     public void moveCursorForward() {
         // DO NOTHING
     }
 
+    /**
+     * Overrides undo command to render it ineffectual
+     */
     @Override
     public void undo() {
         // DO NOTHING
     }
 
+    /**
+     * Overrides redo command to render it ineffectual
+     */
     @Override
     public void redo() {
         // DO NOTHING
     }
 
+    /**
+     * Overrides toString command so that it always returns a String representation without a
+     * cursor (since no text editing will occur)
+     */
     @Override
     public String toString() {
         return toStringFile();
